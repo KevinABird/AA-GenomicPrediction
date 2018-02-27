7ca022840a45187c894bdf7772e5c0d2ca85d0c4
KevinABird <32317851+KevinABird@users.noreply.github.com>
2018-02-27 13:38:03 -0500

    Update 03a_AAS_GAPIT_1_50.R

--- GBLUP_scripts/03a_AAS_GAPIT_1_50.R
+++ GBLUP_scripts/03a_AAS_GAPIT_1_50.R
@@ -17,7 +17,7 @@ myKI <-read.csv("/home/birdk/data/AA_Subset/GAPIT.Kin.VanRaden.csv", head=FALSE)
 
 t=100 #total replicates
 s=1/3 #sample of inference, e.g. set it to 1/5 for five fold cross validation
-Y.raw=as.data.frame(AAabs[,c(1,20)]) #choose a trait
+Y.raw=as.data.frame(AAabs[,c(1,2)]) #choose a trait
 Y.raw=Y.raw[!is.na(Y.raw[,2]),] #Remove missing data
 n=nrow(Y.raw)
 n.missing=round(n*s)
@@ -54,5 +54,5 @@ storage.inf[rep,1]=r.inf
 }#End of for (rep in 1:t)
 storage=cbind(storage.ref,storage.inf)
 colnames(storage)=c("Reference","Inference")
-write.table(storage, file=sprintf("AASubset%s_%s_GAPIT.Cross.Validation.txt",number,colnames(AAabs)[20]), quote = FALSE, sep = "\t", row.names = TRUE,col.names = NA)
+write.table(storage, file=sprintf("AASubset%s_%s_GAPIT.Cross.Validation.txt",number,colnames(AAabs)[2]), quote = FALSE, sep = "\t", row.names = TRUE,col.names = NA)
 }
